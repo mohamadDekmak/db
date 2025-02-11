a85ed355c0c1d6701e77a2d7341c646ba54980e7
mohamad dekmak <126089082+mohamadDekmak@users.noreply.github.com>
2025-02-12 00:28:22 +0200

    .

--- resources/views/pages/hussein.blade.php
+++ resources/views/pages/hussein.blade.php
@@ -6,12 +6,70 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <title>hussein AL Mawla</title>
 </head>
 <body>
 
 <img src="/images/husseinbanner.jpg" alt="" class="object-cover max-h-[500px] w-full">
+<section class="px-2">
+    <h1 class="text-[#3C4592] text-[30px] font-bold text-center">Hussein Al Mawla <br> (Best CEO)</h1>
+    <iframe class="w-full" height="315" src="https://www.youtube.com/embed/gOJdA-hYRFs?si=mHF03IweNPNKSrld"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <div class="swiper text-center">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">B3alemna</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/lkel4.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">bwajhena</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/lkel3.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">By3ml 7alo ma3ju2</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/hussein1.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">W Bnem</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/hussein2.jpg" alt="">
+            </div>
+        </div>
+        <div class="swiper-pagination" style="position: relative;top: 5px"></div>
+    </div>
+    <div>
+        <h1 class="text-[#AD2F36] text-[30px] font-bold">Messages From The Team:</h1>
+        <div>
+            <h1 class="text-[#3C4592] font-bold text-[20px]">Yumna</h1>
+            <p>Happy Birthday my love.</p>
+        </div>
+        <div>
+            <h1 class="text-[#3C4592] font-bold text-[20px]">Moe Haidar</h1>
+            <p>بعد ما فكرت بالموضوع: !!123321 بحبك!</p>
+        </div>
+        <div>
+            <h1 class="text-[#3C4592] font-bold text-[20px]">Maram</h1>
+            <p>لعنة الله على الظالمين</p>
+        </div>
+        <div>
+            <h1 class="text-[#3C4592] font-bold text-[20px]">Mostafa</h1>
+            <p>Töte alle Schwarzen</p>
+        </div>
+    </div>
+</section>
+<script>
+    const Swipert = new Swiper('.swiper', {
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 
+</script>
 
 </body>
 </html>
--- resources/views/pages/mustafa.blade.php
+++ resources/views/pages/mustafa.blade.php
@@ -6,15 +6,66 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <title>Mostafa | Rafikutu</title>
 </head>
 <body>
-<div class="flex justify-center items-center px-2 py-10 h-[200px]"
-         style="background: url('/images/mostafa-banner.jpg'); background-size: cover; background-position: center"
-         id="">
-</div>
+<img src="/images/mostafa-banner.jpg" alt="" class="object-cover max-h-[500px] w-full">
 <div class="bg-orange-100">
     <h1 class="text-black text-4xl font-bold text-center">President of the Milk Club </h1>
 </div>
+<section class="px-2">
+    <div class="swiper text-center">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">ولد في 12\2 و اعلن حبه للبقر و كرهه للسود</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/mustafa14.jpeg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">انضم الى عدة نوادي محاولا تحقيق هدفه ب انشاء نادي الحليب</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/mustafa15.jpeg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">اخر صورة له قبل دخول عالم البرمجة و العمل على fiveStars</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/mustafa12.jpeg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">بعد العمل على FST</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/mustafa13.jpeg" alt="">
+            </div>
+        </div>
+        <div class="swiper-pagination" style="position: relative;top: 5px"></div>
+    </div>
+    <div>
+        <h1 class="text-[#AD2F36] text-[30px] font-bold">Messages From The Team:</h1>
+        <div>
+            <h1 class="text-[#3C4592] font-bold text-[20px]">Yumna</h1>
+            <p>عقبال المية يا ضرّتي.</p>
+        </div>
+        <div>
+            <h1 class="text-[#3C4592] font-bold text-[20px]">Moe Haidar</h1>
+            <p>كل ما شربت الحليب تذكرتك</p>
+        </div>
+        <div>
+            <h1 class="text-[#3C4592] font-bold text-[20px]">Maram</h1>
+            <p> انت شريكنا بتطهير الأرض من نسل ام الجمل</p>
+        </div>
+        <div>
+            <h1 class="text-[#3C4592] font-bold text-[20px]">Hussein</h1>
+            <p>Happy Birthday Son!</p>
+        </div>
+    </div>
+</section>
+<script>
+    const Swipert = new Swiper('.swiper', {
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+</script>
 </body>
 </html>
