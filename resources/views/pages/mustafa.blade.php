696b34502c05fe8e1ab908b4aff8facdf99c0639
YumnaHusseini <129627954+YumnaHusseini@users.noreply.github.com>
2025-02-11 22:36:27 +0200

    banner

--- resources/views/pages/mustafa.blade.php
+++ resources/views/pages/mustafa.blade.php
@@ -6,9 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Document</title>
+    <title>Mostafa | Rafikutu</title>
 </head>
 <body>
-mustafa
+<div class="flex justify-center items-center px-2 py-10 h-[200px]"
+         style="background: url('/images/mostafa-banner.jpg'); background-size: cover; background-position: center"
+         id="">
+</div>
+<div class="bg-orange-100">
+    <h1 class="text-black text-4xl font-bold text-center">President of the Milk Club </h1>
+</div>
 </body>
 </html>
