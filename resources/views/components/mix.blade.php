4a73cf46403e21f0f7c4f9b923f2a8e8ed7dfff5
mohamad dekmak <126089082+mohamadDekmak@users.noreply.github.com>
2025-02-12 12:14:44 +0200

    mix

---
+++ resources/views/components/mix.blade.php
@@ -0,0 +1,53 @@
+<section>
+    <h1></h1>
+    <div class="swiper-m text-center  my-5">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">مصطفى و حسين كانو رفقة</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/mustafawhussein1.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">تعاهدوا انو يحبو و يسنادو بعضهم</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/mustafawhussein2.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">و كتنت الامور بيناتهم متل السمنة عال عسل </h1>
+                <img class="h-[300px] w-full object-cover" src="/images/mustafawhussein3.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold h-[300px] flex justify-center items-center">LA 7AD!!!!<br>!!!!!!</h1>
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">تدخل أندي الشرير بيناتهم</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/andy.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">بديت الخلافات و بطلوا يصلو حد بعض</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/sala2.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">جربو يتناقشو و يلاقو حل بس عبث</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/mh2.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold">هون مصطفى الشرير قرر يعمل انقلاب و ثورة ع حسين</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/enkleb.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <h1 class="text-[#AD2F36] text-[30px] font-bold"> و استولى على املاكه و سيرفيرات الحليب كلها</h1>
+                <img class="h-[300px] w-full object-cover" src="/images/musatfa22.jpg" alt="">
+            </div>
+        </div>
+        <div class="swiper-pagination" style="position: relative;top: 5px"></div>
+    </div>
+
+</section>
+<script>
+    const Swipery = new Swiper('.swiper-m', {
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+</script>
